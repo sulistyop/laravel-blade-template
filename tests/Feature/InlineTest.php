932f892ef6ed49b3bb30a9593219fa1023fa1521
sulistyp <sulisdeveloper@gmail.,com>
2023-08-22 09:20:43 +0700

    inject and blade inline

---
+++ tests/Feature/InlineTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Blade;
+use Tests\TestCase;
+
+class InlineTest extends TestCase
+{
+    public function testInlineBladeTemplate()
+    {
+        $response = Blade::render('Hello {{$name}}', ["name" => "Sulis"]);
+        self::assertEquals("Hello Sulis", $response);
+
+    }
+
+}
